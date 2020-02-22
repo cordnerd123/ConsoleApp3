42b74a83aad587d7bdcf2f6b36b653e848871ed0
cordnerd123 <takamine16@gmail.com>
2020-02-22 17:16:51 -0500

    fixes for 1 and 2, and recommended fixes for 3

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,34 +8,37 @@ namespace ConsoleApp3
 
         {
             //Console.WriteLine("Question 1");
-            //int[] series = { 5, 6, 6, 9, 9, 12 };
-
+            //int[] l1 = { 5, 6, 6, 9, 9, 12 };
             //int target = 9;
-            //string q1 = string.Join(",", targetRange(series, target));
+            //int[] r = TargetRange(l1, target);
+            //Console.WriteLine("[{0},{1}]", r[0],r[1]);   //this prints each array bucket at 0 and 1 respectively
 
-            //Console.WriteLine(q1);
-
-
-            Console.WriteLine("Question 2");
-            string s = "University of South Florida";
-            string rs = StringReverse(s);
-            Console.WriteLine(rs);
+            //note 1 of the assignment said we were not allowed to adjust the method's declaration...this includes return
+            //I changed it back to what prof had and re-adjusted the write so that it would work with an int array
+            //string q1 = string.Join(",", targetRange(, target));
 
 
-            //Console.WriteLine("Question 3");
-            //int[] l2 = new int[] { 40, 40 };
-            //int sum = MinimumSum(l2);
-            //Console.WriteLine(sum);
+
+            //Console.WriteLine("Question 2");
+            //string s = "University of South Florida";
+            //string rs = StringReverse(s);
+            //Console.WriteLine(rs);
+
+
+            Console.WriteLine("Question 3");
+            int[] l2 = new int[] {4,5,6,9};  // this should be 24, instead it is 26 because we are adding 1 to 5 & 6 when we dont' need to
+            int sum = MinimumSum(l2);
+            Console.WriteLine(sum);
 
         }
 
-        static int[] targetRange(int[] series, int target)
+        static int[] TargetRange(int[] l1, int target)
         {
             int[] arr = new int[2] { -1, -1 };
 
-            for (int i = 0; i < series.Length; i++)
+            for (int i = 0; i < l1.Length; i++)
             {
-                if (series[i] == target)
+                if (l1[i] == target)
                 {
                     if (arr[0] == -1)
                     {
@@ -64,28 +67,18 @@ namespace ConsoleApp3
                 ltr = s[i].ToString();
                 // Here the letters of the string s are saved and transformed into a string, with that we can add the other letters inside the rs
 
-
-
-
-
                 if (ltr == " ")
                 {
                     rs += tmp + " ";
-                    tmp = "";
-                   
+                    tmp = "";  
                 }
                 else
                 {
                     tmp = ltr + tmp;
-
-
-
                 }
             }
-
-
-            return rs;
-
+            rs +=tmp;  //this needs to be in here otherwise last word never gets put into rs
+            return tmp;
         }
 
 
@@ -93,6 +86,13 @@ namespace ConsoleApp3
 
         public static int MinimumSum(int[] l2)
         {
+
+            //needs something along the line of
+            //    check first position to see if it is equal to next position
+            //    if it is equal, then increment next position by 1
+            //    if it is not equal then add current position to sum
+            //    move on to the next array position.
+
             int sum = 0;
             for (int i = 0; i < l2.Length; i++) //Here we wanted to create a loop that will growth till i was less than l2
             {
